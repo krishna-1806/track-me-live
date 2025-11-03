fd4cf1b2a24e5e73d3aed425beb618f511567b79
krishna-1806 <tejak1817@gmail.com>
2025-11-03 23:46:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background: #f4f4f4;
+}
+
+h2 {
+  background: #2b6cb0;
+  color: white;
+  padding: 10px;
+  margin: 0;
+}
+
+#map {
+  height: 90vh;
+  width: 100%;
+}
